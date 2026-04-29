01c638182037596c0fd584d9b76231447b31fcea
Sanskar Chavare <sanskarchavare989@gmail.com>
2026-04-29 10:01:47 +0530

    components

---
+++ second-project/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className="card">
+            <h1>sanskar chavare</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, eius.</p>
+        </div>
+    )
+}
+
+export default Card
